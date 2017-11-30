752a08a6152ef9a52f57037a94be5364a95c2cd4
wuxiaoxiaoer <294645581@qq.com>
2017-12-01 00:00:38 +0800

    wlw 通过事件找相关的文章objectSimilarity

--- src/com/sicdlib/entity/ArticleSimilarEntity.java
+++ src/com/sicdlib/entity/ArticleSimilarEntity.java
@@ -5,13 +5,26 @@ package com.sicdlib.entity;
  */
 public class ArticleSimilarEntity {
     private String articleSimilarId;
-<<<<<<< HEAD
-    private String similarDegree;
-=======
     private double similarDegree;
     /*表之间的映射*/
     private ArticleEntity articleEntityOne;
     private ArticleEntity articleEntityTwo;
+
+    public String getArticleSimilarId() {
+        return articleSimilarId;
+    }
+
+    public void setArticleSimilarId(String articleSimilarId) {
+        this.articleSimilarId = articleSimilarId;
+    }
+
+    public double getSimilarDegree() {
+        return similarDegree;
+    }
+
+    public void setSimilarDegree(double similarDegree) {
+        this.similarDegree = similarDegree;
+    }
 
     public ArticleEntity getArticleEntityOne() {
         return articleEntityOne;
@@ -28,31 +41,6 @@ public class ArticleSimilarEntity {
     public void setArticleEntityTwo(ArticleEntity articleEntityTwo) {
         this.articleEntityTwo = articleEntityTwo;
     }
->>>>>>> liuyan
-
-    public String getArticleSimilarId() {
-        return articleSimilarId;
-    }
-
-    public void setArticleSimilarId(String articleSimilarId) {
-        this.articleSimilarId = articleSimilarId;
-    }
-
-<<<<<<< HEAD
-    public String getSimilarDegree() {
-        return similarDegree;
-    }
-
-    public void setSimilarDegree(String similarDegree) {
-=======
-    public double getSimilarDegree() {
-        return similarDegree;
-    }
-
-    public void setSimilarDegree(double similarDegree) {
->>>>>>> liuyan
-        this.similarDegree = similarDegree;
-    }
 
     @Override
     public boolean equals(Object o) {
@@ -61,29 +49,16 @@ public class ArticleSimilarEntity {
 
         ArticleSimilarEntity that = (ArticleSimilarEntity) o;
 
-<<<<<<< HEAD
-        if (articleSimilarId != null ? !articleSimilarId.equals(that.articleSimilarId) : that.articleSimilarId != null)
-            return false;
-        if (similarDegree != null ? !similarDegree.equals(that.similarDegree) : that.similarDegree != null)
-            return false;
-
-        return true;
-=======
         if (Double.compare(that.similarDegree, similarDegree) != 0) return false;
         if (articleSimilarId != null ? !articleSimilarId.equals(that.articleSimilarId) : that.articleSimilarId != null)
             return false;
         if (articleEntityOne != null ? !articleEntityOne.equals(that.articleEntityOne) : that.articleEntityOne != null)
             return false;
         return articleEntityTwo != null ? articleEntityTwo.equals(that.articleEntityTwo) : that.articleEntityTwo == null;
->>>>>>> liuyan
     }
 
     @Override
     public int hashCode() {
-<<<<<<< HEAD
-        int result = articleSimilarId != null ? articleSimilarId.hashCode() : 0;
-        result = 31 * result + (similarDegree != null ? similarDegree.hashCode() : 0);
-=======
         int result;
         long temp;
         result = articleSimilarId != null ? articleSimilarId.hashCode() : 0;
@@ -91,7 +66,6 @@ public class ArticleSimilarEntity {
         result = 31 * result + (int) (temp ^ (temp >>> 32));
         result = 31 * result + (articleEntityOne != null ? articleEntityOne.hashCode() : 0);
         result = 31 * result + (articleEntityTwo != null ? articleEntityTwo.hashCode() : 0);
->>>>>>> liuyan
         return result;
     }
 }
--- src/com/sicdlib/entity/KeywordEntity.java
+++ src/com/sicdlib/entity/KeywordEntity.java
@@ -6,21 +6,9 @@ package com.sicdlib.entity;
 public class KeywordEntity {
     private String keywordId;
     private String keyword;
-<<<<<<< HEAD
-    private String weight;
-=======
     private double weight;
     /*表之间关联映射*/
     private ObjectEntity objectEntity;
-
-    public ObjectEntity getObjectEntity() {
-        return objectEntity;
-    }
-
-    public void setObjectEntity(ObjectEntity objectEntity) {
-        this.objectEntity = objectEntity;
-    }
->>>>>>> liuyan
 
     public String getKeywordId() {
         return keywordId;
@@ -38,20 +26,20 @@ public class KeywordEntity {
         this.keyword = keyword;
     }
 
-<<<<<<< HEAD
-    public String getWeight() {
-        return weight;
-    }
-
-    public void setWeight(String weight) {
-=======
     public double getWeight() {
         return weight;
     }
 
     public void setWeight(double weight) {
->>>>>>> liuyan
         this.weight = weight;
+    }
+
+    public ObjectEntity getObjectEntity() {
+        return objectEntity;
+    }
+
+    public void setObjectEntity(ObjectEntity objectEntity) {
+        this.objectEntity = objectEntity;
     }
 
     @Override
@@ -61,27 +49,14 @@ public class KeywordEntity {
 
         KeywordEntity that = (KeywordEntity) o;
 
-<<<<<<< HEAD
-        if (keywordId != null ? !keywordId.equals(that.keywordId) : that.keywordId != null) return false;
-        if (keyword != null ? !keyword.equals(that.keyword) : that.keyword != null) return false;
-        if (weight != null ? !weight.equals(that.weight) : that.weight != null) return false;
-
-        return true;
-=======
         if (Double.compare(that.weight, weight) != 0) return false;
         if (keywordId != null ? !keywordId.equals(that.keywordId) : that.keywordId != null) return false;
         if (keyword != null ? !keyword.equals(that.keyword) : that.keyword != null) return false;
         return objectEntity != null ? objectEntity.equals(that.objectEntity) : that.objectEntity == null;
->>>>>>> liuyan
     }
 
     @Override
     public int hashCode() {
-<<<<<<< HEAD
-        int result = keywordId != null ? keywordId.hashCode() : 0;
-        result = 31 * result + (keyword != null ? keyword.hashCode() : 0);
-        result = 31 * result + (weight != null ? weight.hashCode() : 0);
-=======
         int result;
         long temp;
         result = keywordId != null ? keywordId.hashCode() : 0;
@@ -89,7 +64,6 @@ public class KeywordEntity {
         temp = Double.doubleToLongBits(weight);
         result = 31 * result + (int) (temp ^ (temp >>> 32));
         result = 31 * result + (objectEntity != null ? objectEntity.hashCode() : 0);
->>>>>>> liuyan
         return result;
     }
 }
